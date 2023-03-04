d4e22ef0e140e03b880fe6d3b795fdee2cf1a858
TeodorDre <swen295@gmail.com>
2023-03-04 13:52:59 +0300

    feat(auth): add auth

---
+++ app/code/store/session-store.ts
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia';
+
+type PopupComponentName = '';
+
+const LAYOUT_STORE_NAME = 'layout';
+
+const useLayoutStore = defineStore(LAYOUT_STORE_NAME, () => {
+});
+
+export default useLayoutStore;
